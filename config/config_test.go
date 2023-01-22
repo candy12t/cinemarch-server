c9d688aa8fe9a0ba418d9dd34ea6114f596ce4f4
Takumi Kanada <candy12t@gmail.com>
2023-01-22 22:44:42 +0900

    Add: test config.HTTPPort()

--- config/config_test.go
+++ config/config_test.go
@@ -1,6 +1,10 @@
-package config
+package config_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/candy12t/cinema-search-server/config"
+)
 
 func TestDSN(t *testing.T) {
 	tests := []struct {
@@ -15,9 +19,30 @@ func TestDSN(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := DSN()
+			got := config.DSN()
 			if got != tt.want {
 				t.Errorf("DNS() got is %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHTTPPort(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "be able to get HTTP_PORT from environment variable",
+			want: "8080",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := config.HTTPPort()
+			if got != tt.want {
+				t.Errorf("HTTPPort() got is %v, want %v", got, tt.want)
 			}
 		})
 	}
