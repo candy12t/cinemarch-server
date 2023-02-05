cd0f65051cc762b5e26b589bcd9dd3f8f20e26f9
Takumi Kanada <candy12t@gmail.com>
2023-02-05 22:44:23 +0900

    Update: table schema

--- _tools/mysql/schema.sql
+++ _tools/mysql/schema.sql
@@ -2,42 +2,39 @@ CREATE DATABASE IF NOT EXISTS `cinemarch`;
 USE `cinemarch`;
 
 CREATE TABLE IF NOT EXISTS `cinemas` (
-  `id` VARCHAR(255) NOT NULL,
-  `name` VARCHAR(255) NOT NULL,
+  `id` VARCHAR(64) NOT NULL,
+  `name` VARCHAR(64) NOT NULL,
+  `prefecture` VARCHAR(16) NOT NULL,
   `address` VARCHAR(255) NOT NULL,
-  `url` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS `screen_movies` (
-  `id` VARCHAR(255) NOT NULL,
-  `cinema_id` VARCHAR(255) NOT NULL,
-  `movie_id` VARCHAR(255) NOT NULL,
-  `start_time` DATETIME NOT NULL,
-  `end_time` DATETIME NOT NULL,
+  `web_site_url` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `screen_movie` (`cinema_id`, `movie_id`, `start_time`, `end_time`)
+  UNIQUE KEY `uniq_cinema` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `movies` (
-  `id` VARCHAR(255) NOT NULL,
-  `title` VARCHAR(255) NOT NULL,
+  `id` VARCHAR(64) NOT NULL,
+  `title` VARCHAR(64) NOT NULL,
   `release_date` DATETIME NOT NULL,
-  `release_status` VARCHAR(255) NOT NULL,
+  `release_status` VARCHAR(16) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_movie` (`title`, `release_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `screen_movies` (
+  `id` VARCHAR(64) NOT NULL,
+  `cinema_id` VARCHAR(64) NOT NULL,
+  `movie_id` VARCHAR(64) NOT NULL,
+  `screen_type` VARCHAR(32),
+  `subtitle_or_dubbing` VARCHAR(32),
+  `three_d` BOOLEAN,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unqi_screen_movie` (`cinema_id`, `movie_id`, `screen_type`, `subtitle_or_dubbing`, `three_d`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `screen_schedules` (
+  `id` VARCHAR(64) NOT NULL,
+  `screen_movie_id` VARCHAR(64) NOT NULL,
+  `start_time` DATETIME NOT NULL,
+  `end_time` DATETIME NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS `screen_movie_screen_types` (
-  `id` VARCHAR(255) NOT NULL,
-  `screen_movie_id` VARCHAR(255) NOT NULL,
-  `screen_type_id` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `movie_type` (`screen_movie_id`, `screen_type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS `screen_types` (
-  `id` VARCHAR(255) NOT NULL,
-  `name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
